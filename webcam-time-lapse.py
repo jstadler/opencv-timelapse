7c1604b180356159317d7b5f1f79f4a4ae7777cc
Jordan Stadler <jordan.stadler@gmail.com>
2014-03-04 16:37:35 -0500

    added delay and folder variables to script

--- webcam-time-lapse.py
+++ webcam-time-lapse.py
@@ -1,21 +1,28 @@
 import numpy as np
 import cv2
+import time
+
+# time delay between frames
+delay = 2.5
+
+# folder to write to
+folder = ''
 
 cap = cv2.VideoCapture(0)
 # 2304 x 1296 gets me 1280x720
 cap.set(4, 2304.0)
 cap.set(3, 1296.0)
-
-print str(cap.get(3)),str(cap.get(4))
+#print str(cap.get(3)),str(cap.get(4))
 
 ret, frame = cap.read()
 count = 0
 while(1):
     ret, frame = cap.read()
     frame_num = "%08d" % (count,)
-    cv2.imwrite('frames/' + frame_num + '.jpg', frame)
+    cv2.imwrite(folder + frame_num + '.jpg', frame)
     k = cv2.waitKey(1)
     count = count + 1
+    time.sleep(delay)
 
 cv2.destroyAllWindows()
 cap.release()
